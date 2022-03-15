60ca1da6995242b99981b3bae3a867fa38f9a088
Samuel <62802232+Sanivaldo@users.noreply.github.com>
2022-03-15 18:44:16 -0300

    Add files via upload

---
+++ BASE_CNES.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import csv
+import pandas as pd
+import wget
+import zipfile
+
+
+# In[11]:
+
+
+url = 'ftp://ftp.datasus.gov.br/cnes/BASE_DE_DADOS_CNES_202201.ZIP'
+csv = wget.download(url)
+
+
+# In[61]:
+
+
+with zipfile.ZipFile("C:\\Users\\samue\\Documents\\BASE_DE_DADOS_CNES_202201 (1).ZIP") as z:
+    print(*z.namelist(),sep="\n")
+
+
+# In[65]:
+
+
+with zipfile.ZipFile("C:\\Users\\samue\\Documents\\BASE_DE_DADOS_CNES_202201 (1).ZIP") as z:
+    with z.open ('tbEstabelecimento202201.csv')as f:
+         microdados_cnae = pd.read_csv(f,sep=';',encoding = 'UTF-8')
+
+
+# In[66]:
+
+
+microdados_cnae.head()
+
+
+# In[67]:
+
+
+microdados_cnae.columns.values
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
